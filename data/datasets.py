68a5a083b9506f7114bd582c4bd1ae25210d41ab
simonwey <1278788005@qq.com>
2022-04-12 01:50:41 +0800

    'fixbaseline'

--- data/datasets.py
+++ data/datasets.py
@@ -21,33 +21,30 @@ def dataset_folder(opt, root):
 
 
 def binary_dataset(opt, root):
-    if opt.isTrain:
-        crop_func = transforms.RandomCrop(opt.cropSize)
-    elif opt.no_crop:
-        crop_func = transforms.Lambda(lambda img: img)
+    composeList = []
+    if not opt.isTrain and opt.no_resize:
+        pass
     else:
-        crop_func = transforms.CenterCrop(opt.cropSize)
+        composeList.append(CustomResize(opt))
+
+    composeList.append(DataAugment(opt))
+
+    if opt.isTrain:
+        composeList.append(transforms.RandomCrop(opt.cropSize))
+    elif opt.no_crop:
+        pass
+    else:
+        composeList.append(transforms.CenterCrop(opt.cropSize))
 
     if opt.isTrain and not opt.no_flip:
-        flip_func = transforms.RandomHorizontalFlip()
-    else:
-        flip_func = transforms.Lambda(lambda img: img)
+        composeList.append(transforms.RandomHorizontalFlip())
 
-    if not opt.isTrain and opt.no_resize:
-        rz_func = transforms.Lambda(lambda img: img)
-    else:
-        rz_func = transforms.Lambda(lambda img: custom_resize(img, opt))
-
-    dset = datasets.ImageFolder(
-            root,
-            transforms.Compose([
-                rz_func,
-                transforms.Lambda(lambda img: data_augment(img, opt)),
-                crop_func,
-                flip_func,
-                transforms.ToTensor(),
-                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            ]))
+    composeList.extend([transforms.ToTensor(),
+                        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                        ])
+    dset = datasets.ImageFolder(root,
+                                transforms.Compose(composeList)
+                                )
     return dset
 
 
@@ -65,19 +62,23 @@ class FileNameDataset(datasets.ImageFolder):
         return path
 
 
-def data_augment(img, opt):
-    img = np.array(img)
+class DataAugment(object):
+    def __init__(self, opt):
+        self.opt = opt
 
-    if random() < opt.blur_prob:
-        sig = sample_continuous(opt.blur_sig)
-        gaussian_blur(img, sig)
+    def __call__(self, img):
+        img = np.array(img)
 
-    if random() < opt.jpg_prob:
-        method = sample_discrete(opt.jpg_method)
-        qual = sample_discrete(opt.jpg_qual)
-        img = jpeg_from_key(img, qual, method)
+        if random() < self.opt.blur_prob:
+            sig = sample_continuous(self.opt.blur_sig)
+            gaussian_blur(img, sig)
 
-    return Image.fromarray(img)
+        if random() < self.opt.jpg_prob:
+            method = sample_discrete(self.opt.jpg_method)
+            qual = sample_discrete(self.opt.jpg_qual)
+            img = jpeg_from_key(img, qual, method)
+
+        return Image.fromarray(img)
 
 
 def sample_continuous(s):
@@ -96,17 +97,17 @@ def sample_discrete(s):
 
 
 def gaussian_blur(img, sigma):
-    gaussian_filter(img[:,:,0], output=img[:,:,0], sigma=sigma)
-    gaussian_filter(img[:,:,1], output=img[:,:,1], sigma=sigma)
-    gaussian_filter(img[:,:,2], output=img[:,:,2], sigma=sigma)
+    gaussian_filter(img[:, :, 0], output=img[:, :, 0], sigma=sigma)
+    gaussian_filter(img[:, :, 1], output=img[:, :, 1], sigma=sigma)
+    gaussian_filter(img[:, :, 2], output=img[:, :, 2], sigma=sigma)
 
 
 def cv2_jpg(img, compress_val):
-    img_cv2 = img[:,:,::-1]
+    img_cv2 = img[:, :, ::-1]
     encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), compress_val]
     result, encimg = cv2.imencode('.jpg', img_cv2, encode_param)
     decimg = cv2.imdecode(encimg, 1)
-    return decimg[:,:,::-1]
+    return decimg[:, :, ::-1]
 
 
 def pil_jpg(img, compress_val):
@@ -121,6 +122,8 @@ def pil_jpg(img, compress_val):
 
 
 jpeg_dict = {'cv2': cv2_jpg, 'pil': pil_jpg}
+
+
 def jpeg_from_key(img, compress_val, key):
     method = jpeg_dict[key]
     return method(img, compress_val)
@@ -130,6 +133,12 @@ rz_dict = {'bilinear': Image.BILINEAR,
            'bicubic': Image.BICUBIC,
            'lanczos': Image.LANCZOS,
            'nearest': Image.NEAREST}
-def custom_resize(img, opt):
-    interp = sample_discrete(opt.rz_interp)
-    return TF.resize(img, opt.loadSize, interpolation=rz_dict[interp])
+
+
+class CustomResize(object):
+    def __init__(self, opt):  # snr, p 是要传入的多个参数
+        self.opt = opt
+
+    def __call__(self, img):  # __call__函数还是只有一个参数传入
+        interp = sample_discrete(self.opt.rz_interp)
+        return TF.resize(img, self.opt.loadSize, interpolation=rz_dict[interp])
