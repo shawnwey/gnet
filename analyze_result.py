341a26d018d0da0dee501e37a47235e223ba9e7c
simonwey <1278788005@qq.com>
2022-05-07 21:38:23 +0800

    'analyze'

---
+++ analyze_result.py
@@ -0,0 +1,15 @@
+import argparse
+from core.analyze import analyze_net_pklResults
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    # 命名规定：网络名称（用net拼接）-其他实验因素，下划线区分
+    parser.add_argument('--exp_id', type=str, default='EfficientNetB4-baseline')
+    args, _ = parser.parse_known_args()
+
+    analyze_net_pklResults(args.exp_id)
+
+if __name__=='__main__':
+    main()
